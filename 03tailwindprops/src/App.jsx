44e819562e6cb53033c4dd0b8f74de4c4c0b30fb
unknown <ajodzsoni@gmail.com>
2024-07-22 11:10:53 +0200

    Project 5

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -7,6 +7,17 @@ import Card from './components/Card'
 function App() {
   const [count, setCount] = useState(0)
 
+  let myObj = {
+    name: 'Jonathan',
+    age: 25,
+    address:{
+      city: 'Altstatten',
+      state: 'SG',
+      country: "Switzerland"
+    }
+  }
+  let newArr = [1,2,3,4,5,6,7,8,9]
+
   return (
     <>
       <h1 className='text-3xl bg-green-500 p-3 rounded-md'>Vite with Tailwind</h1>
